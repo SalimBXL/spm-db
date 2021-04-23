47d36616760f371c17f395d0eb830565b9597d9e
Salim A. JOLY <el.salim.salim@gmail.com>
2021-04-23 11:58:07 +0200

    ok

--- app/controllers/matlab_controller.rb
+++ app/controllers/matlab_controller.rb
@@ -2,11 +2,48 @@ class MatlabController < ApplicationController
     before_action :get_camera, only: [:index, :start_matlab]
 
     def index
-        
+        @progression = get_progression(1, 6)
+    end
+
+    def get_zip
+        @progression = get_progression(2, 6)
+    end
+
+    def get_zip_ok
+        @progression = get_progression(3, 6)
+        xterm = "xterm -e"
+        comm = "top ; wait"
+        @value = %x( #{xterm} "#{comm}" )
+    end
+
+    def read_dicom_header
+        @progression = get_progression(4, 6)
+    end
+
+    def read_dicom_header_ok
+        @progression = get_progression(5, 6)
+        xterm = "xterm -e"
+        comm = "top ; wait"
+        @value = %x( #{xterm} "#{comm}" )
     end
 
     def start_matlab
+        @progression = get_progression(6, 6)
 
+        xterm = "xterm -e"
+        comm = "top ; wait"
+
+        @value = %x( #{xterm} "#{comm}" )
+        @wasGood = system( "#{xterm} '#{comm}'" )
+        @wasGood2 = $?
+    end
+
+    def add_pdf_to_db
+        @progression = get_progression(5, 6)
+    end
+
+    def remove_dicom_entry
+        @progression = get_progression(6, 6)
     end
 
     private
@@ -16,6 +53,10 @@ class MatlabController < ApplicationController
         @cameras[1] = "PET-CT"
         @cameras[2] = "PET-MR"
         @camera = params[:camera] if params[:camera]
+    end
+
+    def get_progression(etape, etapes)
+        ( (100/etapes) * (etape-1) )
     end
     
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,16 @@ Rails.application.routes.draw do
   root "home#index"
 
   get "/matlab", to: "matlab#index"
+
+  get "/get_zip", to: "matlab#get_zip"
+  get "/get_zip_ok", to: "matlab#get_zip_ok"
+
+  get "/read_dicom_header", to: "matlab#read_dicom_header"
+  get "/read_dicom_header_ok", to: "matlab#read_dicom_header_ok"
+
   get "/start_matlab", to: "matlab#start_matlab"
+  get "/add_pdf_to_db", to: "matlab#add_pdf_to_db"
+  get "/remove_dicom_entry", to: "matlab#remove_dicom_entry"
 
 
   resources :spms
