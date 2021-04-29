6805403c0eee2f63938712fb10a320ee362f3fed
Salim A. JOLY <el.salim.salim@gmail.com>
2021-04-29 13:13:20 +0200

    test

--- app/controllers/matlab_controller.rb
+++ app/controllers/matlab_controller.rb
@@ -60,6 +60,7 @@ class MatlabController < ApplicationController
 
                     puts "***** CONTROLLER - patient_study : #{patient_study}"
 
+                    patient_study_date = patient_study["MainDicomTags"]
                     patient_study_date = patient_study["StudyDate"]
 
                     puts "***** CONTROLLER - patient_study_date : #{patient_study_date}"
