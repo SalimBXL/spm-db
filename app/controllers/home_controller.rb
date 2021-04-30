152fbaa325f31eb08859c5f4bc385799072f0b99
Salim A. JOLY <el.salim.salim@gmail.com>
2021-04-30 03:55:53 +0200

    admin mode

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,13 @@ class HomeController < ApplicationController
 
     def index
         @lasts = Spm.last(8).reverse
+
+        # Local server IP address
+        ip = Socket.ip_address_list.detect{|intf| intf.ipv4_private?}
+        @client_address = ip.ip_address
+
+        # Client IP address
+        @server_address = request.remote_ip
     end
     
 end
